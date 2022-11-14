192d7e821fda4b3821a81aceb5b4b262c8086726
Sasha Stafford <sasha.stafford@icloud.com>
2022-11-13 22:57:26 -0500

    menutoggle styles

--- components/MenuToggle.tsx
+++ components/MenuToggle.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/MenuToggle.module.css'
 export default function MenuToggle() {
   return (
     <input
-      className={styles.sideMenu}
+      className={styles.menuToggle}
       type="checkbox"
       aria-label="open the menu"
     />
