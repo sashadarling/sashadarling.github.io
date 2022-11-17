2bd3f236ec29ee9a7593c493ccd2e09024885853
Sasha Stafford <sasha.stafford@icloud.com>
2022-11-17 16:19:02 -0500

    contact form styles and fixed hide/show menu on route change

--- components/MenuToggle.tsx
+++ components/MenuToggle.tsx
@@ -1,11 +1,29 @@
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
 import styles from '../styles/components/MenuToggle.module.css'
 
+
 export default function MenuToggle() {
+  const router = useRouter()
+
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked)
+  }
+
+  useEffect(() => {
+    setChecked(false)
+  }, [router.asPath])
+
   return (
     <input
       className={styles.menuToggle}
       type="checkbox"
+      checked={checked}
       aria-label="open the menu"
+      onChange={handleChange}
     />
   )
 }
